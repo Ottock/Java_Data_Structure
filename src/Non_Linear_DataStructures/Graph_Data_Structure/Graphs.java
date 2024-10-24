ab287697e69e5f44d3725599fe23103829fd754a
22.00571-4 <22.00571-4@maua.br>
2024-10-24 09:06:40 -0300

    Graphs Data Structures criada

---
+++ src/Non_Linear_DataStructures/Graph_Data_Structure/Graphs.java
@@ -0,0 +1,20 @@
+// src/Non_Linear_DataStructures/Graph_Data_Structure/Graphs.java
+package Non_Linear_DataStructures.Graph_Data_Structure;
+
+public class Graphs {
+    // Atributos
+
+    // Clase Interna Node
+    public static class GraphNode {
+        // Atributos
+        private int Value;
+
+        // Construtor
+        public GraphNode(int value){
+            this.Value = value;
+        }
+
+        // Funções
+    }
+
+}
---
+++ src/Non_Linear_DataStructures/Graph_Data_Structure/GraphsMain.java
@@ -0,0 +1,5 @@
+// src/Non_Linear_DataStructures/Graph_Data_Structure/GraphsMain.java
+package Non_Linear_DataStructures.Graph_Data_Structure;
+
+public class GraphsMain {
+}
